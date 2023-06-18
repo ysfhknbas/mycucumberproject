57b636086c2aa0e89c42cb80bb0c7f1c5587c31b
Yusuf Bas <yusufhbas@yahoo.com>
2023-06-17 23:05:51 -0400

    new commit

--- src/test/java/stepdefinitions/GoogleStepDefs.java
+++ src/test/java/stepdefinitions/GoogleStepDefs.java
@@ -1,8 +1,37 @@
 package stepdefinitions;
 
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import org.openqa.selenium.Keys;
+import pages.GooglePage;
+import utilities.ConfigReader;
+import utilities.Driver;
+
 public class GoogleStepDefs {
 
+    GooglePage googlePage = new GooglePage();
+    @Given("user is on the google home page")
+    public void user_is_on_the_google_home_page() {
 
+        Driver.getDriver().get(ConfigReader.getProperty("google_url"));
 
+        try {
+            googlePage.popUpAccept.click();
+        }catch (Exception e){
+
+        }
+    }
+    @Given("user search for iPhone")
+    public void user_search_for_i_phone() {
+
+        googlePage.searchbox.sendKeys("iPhone", Keys.ENTER);
+    }
+    @Then("verify the page title contains iPhone")
+    public void verify_the_page_title_contains_i_phone() {
+
+        Assert.assertTrue(Driver.getDriver().getTitle().contains("iPhone"));
+    }
 
 }
